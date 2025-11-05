37fcdf237f8466c956a8987645cbae20926962eb
Tyler-Arciniaga <tyarciniaga@gmail.com>
2025-11-05 01:08:35 -0500

    completed part 2 of broadcast challenge, created multi-node system with message propagation

--- broadcast/main.go
+++ broadcast/main.go
@@ -11,6 +11,8 @@ func main() {
 	n := maelstrom.NewNode()
 
 	var messages []float64
+	var neighbors []string
+
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var body map[string]any
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
@@ -23,7 +25,39 @@ func main() {
 		messages = append(messages, newMessage)
 		delete(body, "message")
 
+		propMsgBody := make(map[string]any)
+		propMsgBody["src"] = n.ID()
+		propMsgBody["message"] = newMessage
+		propMsgBody["type"] = "propagate"
+		for _, nei := range neighbors {
+			n.Send(nei, propMsgBody)
+		}
+
 		return n.Reply(msg, body)
+	})
+
+	n.Handle("propagate", func(msg maelstrom.Message) error {
+		body, err := UnmarshalReqBody(msg)
+		if err != nil {
+			return err
+		}
+
+		sender := body["src"]
+		newMessage := body["message"].(float64)
+		messages = append(messages, newMessage)
+
+		propMsg := make(map[string]any)
+		propMsg["src"] = n.ID()
+		propMsg["message"] = newMessage
+		propMsg["type"] = "propagate"
+
+		for _, nei := range neighbors {
+			if nei != sender {
+				n.Send(nei, propMsg)
+			}
+		}
+
+		return nil
 	})
 
 	n.Handle("read", func(msg maelstrom.Message) error {
@@ -39,15 +73,21 @@ func main() {
 	})
 
 	n.Handle("topology", func(msg maelstrom.Message) error {
-		body, err := UnmarshalReqBody(msg)
+		type TopologyBody struct {
+			Type     string              `json:"type"`
+			Topology map[string][]string `json:"topology"`
+			Msg_id   int                 `json:"msg_id`
+		}
+		var body TopologyBody
+		err := json.Unmarshal(msg.Body, &body)
 		if err != nil {
 			return err
 		}
 
-		body["type"] = "topology_ok"
-		delete(body, "topology")
+		nei := body.Topology[n.ID()]
+		neighbors = append(neighbors, nei...)
 
-		return n.Reply(msg, body)
+		return n.Reply(msg, map[string]any{"type": "topology_ok"})
 	})
 
 	if err := n.Run(); err != nil {
